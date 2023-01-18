dcdc6f5ab41fc85e978bdeae9aed151cc846c79f
BensonAtencio <benson.atencio@g.batstate-u.edu.ph>
2023-01-18 12:55:26 +0800

    intro to data science

---
+++ python/data science/trydatascience.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+# a = np.array([1, 2, 3, 4, 5])
+# b = np.array([6, 7, 8, 9, 10])
+
+# print(a[0])
+# print(b[0])
+
+# c = np.array([
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ])
+
+# print(c.shape)
+
+# x = np.array([1,2,3,4])
+# y = np.array([5,6,7,8])
+
+# # print(x+y)
+
+# np.zeros((number of lists, rows, columns))
+# a = np.zeros((5, 5, 5))
+# print(a)
+
+# a = np.ones((5, 5, 5))
+# print(a)
+
+# a = np.full((5, 5, 5), 7)
+# print(a)
+
+# a = np.empty((5,5,5))
+# print(a)
+
+# a = np.random.random((5, 5))
+# print(a)
+
+# x = np.array([0, 5, 10, 15, 20, 25, 30])
+# a = np.arange(0, 1000, 5)
+# y = x * 2 - x ** 2
+# b = a * 2 - a ** 2
+
+# print(y)
+# print(b)
+
+# x = np.linspace(0, 1000, 5)
+# print(x)
+
+# a = np.array([
+#     [
+#         [1,2,3],
+#         [4,5,6]
+#     ],
+#     [
+#         [10,20,30],
+#         [40,50,60]
+#     ]
+# ], dtype = int) #dtype = float
+
+# print(a)
+# print(a.shape)
+# print(a.size)
+# print(a.ndim)
+# print(a.dtype)
